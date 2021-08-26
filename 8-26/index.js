cec756d45f1f83c46226adc2c2098ebbccd15100
Julian Phillips <julian@julianlp.com>
2021-08-26 16:54:26 -0600

    Class notes from the last two days

---
+++ 8-26/index.js
@@ -0,0 +1,26 @@
+// const canvas = document.getElementById("example");
+// const ctx = canvas.getContext("2d");
+
+// console.log(canvas)
+// console.log(ctx)
+
+
+// fillRect(x, y, width, height); //Draws a filled rectangle.
+// strokeRect(x, y, width, height); //Draws a rectangular outline.
+// clearRect(x, y, width, height); //Clears the specified rectangular area, making it fully transparent.
+
+// i = 0, j = 0
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log({i})
+        console.log({j})   
+    }
+    
+}
+
+let matrix = [
+    [1, 2, 3], // matrix[i][j]
+    [4, 5, 6],
+    [7, 8, 9]
+]
